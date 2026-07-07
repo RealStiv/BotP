6871bc3a3f94250999e49e8053706b43ac122b31
RealStiv <durangostiven444@gmail.com>
2026-07-07 05:49:37 -0400

    Update MongoDB connection URI
    Updated MongoDB connection URI to include retryWrites and SSL options.

--- config.py
+++ config.py
@@ -18,7 +18,6 @@ ADMIN_ID = 8831594637
 # 📢 ID DEL CANAL DONDE LLEGARÁN LOS LOGS
 CHANNEL_LOGS = -1004364812976  # 👈 REEMPLAZA ESTO CON EL ID DE TU CANAL
 
-# 🍃 CONEXIÓN MONGODB
-# ⚠️ IMPORTANTE: Pon tu URL de conexión aquí
-MONGO_URI = "mongodb+srv://robertotinta1957_db_user:Badak9080.@novobot.yfkxqnh.mongodb.net/"
+# 🍃 CONEXIÓN MONGODB ✅ ACTUALIZADA
+MONGO_URI = "mongodb+srv://robertotinta1957_db_user:Badak9080.@novobot.yfkxqnh.mongodb.net/?retryWrites=true&w=majority&ssl=true&tlsAllowInvalidCertificates=true"
 MONGO_DB_NAME = "NovoBot"
